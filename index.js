f5296eb6ccf3f2e1a1b6b85fde489e42e8dd6040
Denis Oblogin <ukrbublik@gmail.com>
2017-01-06 19:10:52 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Simple wrapper for net.Socket to send/receive JSON objects and Buffer streams
+ *
+ * @author ukrbublik
+ */
+
+const ReadBufferStream = require('./lib/BufferStream').ReadBufferStream;
+const WriteBufferStream = require('./lib/BufferStream').WriteBufferStream;
+const attachStreamToSocket = require('./lib/BufferStream').attachStreamToSocket;
+const attachSocketToStream = require('./lib/BufferStream').attachSocketToStream;
+const TcpSocket = require('./lib/TcpSocket');
+
+module.exports = {
+  ReadBufferStream: ReadBufferStream,
+  WriteBufferStream: WriteBufferStream,
+  TcpSocket: TcpSocket,
+};
